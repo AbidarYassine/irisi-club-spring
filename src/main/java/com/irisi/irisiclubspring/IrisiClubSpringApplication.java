de8048d6dd307d14a692ec5458661855338a0921
AbidarYassine <yassine.abidar@edu.uca.ma>
2022-04-16 17:20:22 +0000

    Init project

---
+++ src/main/java/com/irisi/irisiclubspring/IrisiClubSpringApplication.java
@@ -0,0 +1,13 @@
+package com.irisi.irisiclubspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IrisiClubSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IrisiClubSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/irisi/irisiclubspring/IrisiClubSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irisi.irisiclubspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IrisiClubSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
